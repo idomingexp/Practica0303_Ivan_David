7a036be8ae437279f183f90c73104f99c0291c25
idomingexp <idomingexp@educacion.navarra.es>
2024-01-30 13:30:32 +0100

    Update Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -2,7 +2,7 @@ import pygame
 
 
 pygame.init()
-ventana = pygame.display.set_mode((640,480))
+ventana = pygame.display.set_mode((1080,720))
 pygame.display.set_caption("ejercicio 2")
 
 # Crea el objeto pelota
@@ -12,7 +12,8 @@ ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
 
 # Inicializo los valores con los que se van a mover la pelota
-speed = [4,4]
+#(Primer dato--> Velocidad izqda/drcha, Segundo dato--> Velocidad Arriba/Abajo)
+speed = [3,3]
 
 # Pongo la pelota en el origen de coordenadas
 ballrect.move_ip(0,0)
@@ -29,15 +30,17 @@ while jugando:
     # Compruebo si la pelota llega a los límites de la ventana
     if ballrect.left < 0 or ballrect.right > ventana.get_width():
         speed[0] = -speed[0]
-            
+    #Si llega inviertes la velocidad para que se vea como si estuviera rebotando        
     if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
         speed[1] = -speed[1]
     
-    ventana.fill((252, 243, 207))
+    ventana.fill((39,55,70))
 
     # Dibujo la pelota
     ventana.blit(ball, ballrect)
+    #Actualizamos la pantalla para ver la posición de la pelota
     pygame.display.flip()
-    pygame.time.Clock().tick(60)
+    #FPS a los que va el juego
+    pygame.time.Clock().tick(144)
 
 pygame.quit()
