fd054b3c39c12bd6bc193b44d6349b656039896f
idomingexp <idomingexp@educacion.navarra.es>
2024-02-02 11:20:38 +0100

    Update Ejercicio 3.py

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -11,7 +11,7 @@ ventana.blit(fondo, (0,0))
 #Cargamos la bola
 ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
-speedball = [3,3]
+speed = [3,3]
 ballrect.move_ip(0,0)
 
 # Crea el objeto bate, y obtengo su rectángulo
@@ -20,33 +20,35 @@ baterect = bate.get_rect()
 
 # Pongo el bate en la parte inferior de la pantalla
 # Primer dato es la posición horizontal y segundo la vertical
-baterect.move_ip(540,600)
+baterect.move_ip(600,600)
 
 jugando = True
 while jugando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jugando = False
-
     # Compruebo si se ha pulsado alguna tecla
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         baterect = baterect.move(-4,0)
+        
     if keys[pygame.K_RIGHT]:
         baterect = baterect.move(4,0)
+        
 
     # Compruebo si hay colisión
     if baterect.colliderect(ballrect):
-        speedball[1] = -speedball[1]
+        speed[1] = -speed[1]
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > ventana.get_width():
-        speedball[0] = -speedball[0]
-    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
-        speedball[1] = -speedball[1]
-    if baterect.left < 0 or baterect.right > ventana.get_width():
         speed[0] = -speed[0]
-    if baterect.top < 0 or baterect.bottom > ventana.get_height():
+    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
         speed[1] = -speed[1]
+    
+    if baterect.left < 0:
+       baterect = baterect.move(4,0)
+    if baterect.right > ventana.get_width():
+        baterect = baterect.move(-4,0)
      #Borrar lo que habia en la ventana
     ventana.fill((0,0,0))
      #Ponemos todo el rato el fondo
